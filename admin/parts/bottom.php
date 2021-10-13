d2247f5eeb12e717758663edd0b5a053dd557af3
An <18481195+AnTheMaker@users.noreply.github.com>
2021-10-13 15:47:57 +0200

    update footer

--- admin/parts/bottom.php
+++ admin/parts/bottom.php
@@ -2,7 +2,7 @@
 
   <footer class="p-4 footer bg-base-200 fixed bottom-0 text-base-content footer-center">
     <div>
-      <p>Copyright © <?php echo date('Y'); ?> - An R.  - Fragen? <b>github.com/anthemaker</b></p>
+      <p>Copyright © <?php echo date('Y'); ?> - Anton - Fragen? <b>github.com/anthemaker</b></p>
     </div>
   </footer>
 </body>
